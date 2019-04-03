232ba5567c666f2aaf6018ad98d63301038d6001
halented <hallie.dunn1@gmail.com>
2019-04-03 10:48:29 -0700

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
+import Cell from './Cell'
 
 export default class Matrix extends Component {
-  
+
+  constructor(props) {
+    super(props)
+    // this.state = {
+    //   values: ''
+    // }
+  }
+
   genRow = (vals) => (
-    vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    vals.map(val => <div className="cell"> <Cell value={val} /></div>)
   )
   
   genMatrix = () => (
-    this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+    this.props.values.map(rowVals => <div className="row" key={rowVals.id}>{this.genRow(rowVals)}</div>)
   )
   
   render() {
     return (
-      <div id="matrix">
+      <div id="matrix" key="This one has a key too">
         {this.genMatrix()}
       </div>
     )
   }
-  
 }
+
+Matrix.defaultProps = {
+  values: (() => {
+    const defRow = ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+    return (new Array(10).fill(defRow))
+  })()
+}
+//['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'], ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'], ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'], ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'], ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
